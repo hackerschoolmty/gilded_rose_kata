82526e5b36157a3de5d922d77fbc91443e99fff6
Mayra Cabrera <mcabrera1087@gmail.com>
2016-07-02 11:57:18 -0500

    Add gilded rose kata exercise

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,53 +1,44 @@
+require 'pry'
+
 class GildedRose
-  attr_reader :name, :quality, :days_remaining
+   attr_reader :name, :quality, :days_remaining
+  
 
   def initialize(name, quality, days_remaining)
     @name, @quality, @days_remaining = name, quality, days_remaining
   end
 
   def tick
-    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if @name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality += 1
-        if @name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @days_remaining < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
+    if @name == "normal" && @quality  > 0
+      if @days_remaining > 0 
+        @quality -= 1
+      else
+        @quality -= 2
       end
     end
-    if @name != 'Sulfuras, Hand of Ragnaros'
-      @days_remaining -= 1
+    if @name == "Aged Brie" && @quality < 50
+      @quality += 1 
+      @quality += 1 if @days_remaining <= 0
     end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if @name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
+
+    if @name == 'Backstage passes to a TAFKAL80ETC concert' && @quality <= 50
+      if @days_remaining > 0
+        if @quality > @days_remaining
+          @quality += 3 
+          @quality -= 1 if @days_remaining > 5
+        elsif @quality < @days_remaining
+          @quality += 1
         else
-          @quality = @quality - @quality
+          @quality += 2
         end
       else
-        if @quality < 50
-          @quality += 1
-        end
+        @quality = 0
       end
     end
+
+    @days_remaining -= 1 unless @name == 'Sulfuras, Hand of Ragnaros'
   end
+
+
+
 end
